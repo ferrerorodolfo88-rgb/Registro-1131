1e9b6705faf2b4817d8cc7239d4240fb01fb95ca
ferrerorodolfo88-rgb <ferrerorodolfo88@gmail.com>
2026-02-14 10:50:02 -0300

    Create Firebase-config.js

---
+++ Firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwccZj6pnpcRDmDMacIYHEtNIlFuEpHWc",
+  authDomain: "isnp1131-registros.firebaseapp.com",
+  projectId: "isnp1131-registros",
+  storageBucket: "isnp1131-registros.firebasestorage.app",
+  messagingSenderId: "751672866310",
+  appId: "1:751672866310:web:078b08a62becde0bc77587"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
